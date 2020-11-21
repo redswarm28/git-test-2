f1bacddadfccb935dc913a9562ea202cf22e9d5c
redswarm28 <a.tthazikry097@gmail.com>
2020-11-21 22:45:15 +0700

    Menambahkan list

--- index.php
+++ index.php
@@ -7,5 +7,9 @@
 <body>
 	<h1>Hello, world!</h1>
 	<p>Dolor molestiae eum at sit dolor pariatur. Dolor laudantium velit soluta sunt facilis? Est sit at magnam hic possimus Saepe nam officiis velit fugiat voluptas, odit Unde dignissimos molestiae ipsa</p>
+	<ul>
+		<li>Satu</li>
+		<li>Dua</li>
+	</ul>
 </body>
 </html>
